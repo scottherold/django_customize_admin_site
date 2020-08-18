e2114b209d3df579a2a406ec28330b4c3bb8ffe6
Scott <sherold@mail.usf.edu>
2020-08-18 15:18:17 -0400

    Reordered Movie Model in admin site by creating MovieAdmin in videos/admin.py

--- videos/admin.py
+++ videos/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
+class MovieAdmin(admin.ModelAdmin):
+    """A simple class representing the admin view of Movie Model.
+
+    Attributes:
+        fields (list): Orders the Model fields for display in the django admin
+        site.
+    """
+    fields = ['release_year', 'title', 'length']
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie, MovieAdmin)
