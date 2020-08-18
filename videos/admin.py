08292942945f031662bd85290b29f32d6015b26c
Scott <sherold@mail.usf.edu>
2020-08-18 15:38:46 -0400

    Length made into an editable field on the list view on the admin site by adding the list_editable attribute to the MovieAdmin class.

--- videos/admin.py
+++ videos/admin.py
@@ -18,6 +18,7 @@ class MovieAdmin(admin.ModelAdmin):
     search_fields = ['title','length']
     list_filter = ['release_year', 'length']
     list_display = ['title', 'length', 'release_year']
+    list_editable = ['length']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
