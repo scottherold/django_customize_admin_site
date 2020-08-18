6d49a6674ae570abeb9e5dfaaca30ce322b26686
Scott <sherold@mail.usf.edu>
2020-08-18 15:37:48 -0400

    created a list display for movies on the admin site by adding the list_display attribute to the Movie Admin class

--- videos/admin.py
+++ videos/admin.py
@@ -17,6 +17,7 @@ class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
     search_fields = ['title','length']
     list_filter = ['release_year', 'length']
+    list_display = ['title', 'length', 'release_year']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
